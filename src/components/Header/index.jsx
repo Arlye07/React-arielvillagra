b65ab41bb4aa8dd9a3e4b2dcadcdd5314ca7472d
Arlye07 <arielvillagra7@gmail.com>
2022-12-20 01:06:56 -0300

    git ignore de readme

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import './style.css';
+
+function Header({ numbreOfProducts: nroProducts = 3}) {
+    return(
+        <head className='header'>
+            <div className='companyName'>
+                Art Store {nroProducts}
+            </div>
+        </head>
+    );
+}
+
+export default Header;
