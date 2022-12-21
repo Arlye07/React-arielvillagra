400440fad40703ad8b6a3b1d237e3ba7d6df5d0e
Arlye07 <arielvillagra7@gmail.com>
2022-12-20 22:47:54 -0300

    trabado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
+//import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
 import logo from './logo.svg';
+import { Button } from 'antd';
+import 'antd/dist/reset.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <div className="App">
+    <Button type="primary">Button</Button>
+  </div>
+
     </div>
+    
   );
 }
 
+
+
+ 
+
+
 export default App;
+
--- src/Containers/app.js
+++ src/Containers/app.js
@@ -1,50 +1,50 @@
-import './app.css';
-import { useEffect, useState } from 'react';
-import Header from './Containers/Header';
-import HighLightedContent from './Containers/HighLightedContent';
+// import './app.css';
+// import { useEffect, useState } from 'react';
+// import Header from './Containers/Header';
+// import HighLightedContent from './Containers/HighLightedContent';
 
 
-function MainContainer({children}){
-return(<main>
-    {children}
-</main>
-)
-}
+// function MainContainer({children}){
+// return(<main>
+//     {children}
+// </main>
+// )
+// }
 
-function App (){
-    const [foo, setFoo] = useState (2);
-    const [bar, setBar] = useState (20);
+// function App (){
+//     const [foo, setFoo] = useState (2);
+//     const [bar, setBar] = useState (20);
 
-    useEffect(()=> {
-        console.log('App render userEffect ....');
-        return () =>{
-            document.removeEventListener()
-        }
-    });
-    useEffect(()=>{ 
-    console.log('App render userEffect  una vez....'); 
-},[] );
-useEffect(()=>{ 
-    console.log('App render userEffect  with foo....'); 
-},[foo] );
+//     useEffect(()=> {
+//         console.log('App render userEffect ....');
+//         return () =>{
+//             document.removeEventListener()
+//         }
+//     });
+//     useEffect(()=>{ 
+//     console.log('App render userEffect  una vez....'); 
+// },[] );
+// useEffect(()=>{ 
+//     console.log('App render userEffect  with foo....'); 
+// },[foo] );
 
-    console.log('APP MOUNTED', foo);
+//     console.log('APP MOUNTED', foo);
 
-    return (
-        <div className='App'>
-        <Header />
-        <MainContainer />
-        <HighLightedContent />
-        <NewContainer />
-        <button onClick={()=> setfoo ('fooChanged')}>change</button>
-        <MainContainer />
-        </div>
-    );
-}
-function HighLightedContent(){
-    return ( <section />)
-    <img src="" alt="">
-}
-function NewItems (){}
+//     return (
+//         <div className='App'>
+//         <Header />
+//         <MainContainer />
+//         <HighLightedContent />
+//         <NewContainer />
+//         <button onClick={()=> setfoo ('fooChanged')}>change</button>
+//         <MainContainer />
+//         </div>
+//     );
+// }
+// // function HighLightedContent(){
+// //     return ( <section />)
+// //     <img src="" alt="">
+// // }
+// function NewItems (){}
 
-export default 
+// export default App;
